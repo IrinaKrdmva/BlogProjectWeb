0362f74fb5fbd27e9301c74134d5813daef21469
Irina Karadimova <irinamaple84@gmail.com>
2025-02-13 12:39:08 +0000

    Adding a blog post controller

---
+++ BlogProjectWeb/Controllers/AdminBlogPostsController.cs
@@ -0,0 +1,33 @@
+﻿using BlogProjectWeb.Models.ViewModels;
+using BlogProjectWeb.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BlogProjectWeb.Controllers {
+    public class AdminBlogPostsController : Controller {
+        private readonly ITagRepository tagRepository;
+
+        public AdminBlogPostsController(ITagRepository tagRepository) {
+            this.tagRepository = tagRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Add() {
+
+            //Get tags from repository
+            var tags = await tagRepository.GetAllAsync();
+
+            var model = new AddBlogPostRequest {
+                Tags = tags.Select(x => new SelectListItem { Text = x.Name, Value = x.Id.ToString() })
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddBlogPostRequest addBlogPostRequest) {
+            return RedirectToAction("Add");
+        }
+
+    }
+}
---
+++ BlogProjectWeb/Models/ViewModels/AddBlogPostRequest.cs
@@ -0,0 +1,23 @@
+﻿using BlogProjectWeb.Models.Domain;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BlogProjectWeb.Models.ViewModels {
+    public class AddBlogPostRequest {
+        public string Heading { get; set; } 
+        public string PageTitle { get; set; }
+        public string Content { get; set; }
+        public string ShortDescription { get; set; }
+        public string FeaturedImageUrl { get; set; }
+        public string UrlHandle { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public string Author { get; set; }
+        public bool Visible { get; set; }
+
+        //Display tags
+        public IEnumerable<SelectListItem> Tags { get; set; }
+
+        //Capture or collect the tag
+        public string SelectedTag { get; set; }
+
+    }
+}
