99180137fbc395a09a2bdeed1cf1826ca450b35d
Irina Karadimova <irinamaple84@gmail.com>
2025-02-19 12:43:19 +0000

    Adding the Update BlogPost functionality with relation to the tags

--- BlogProjectWeb/Repositories/BlogPostRepository.cs
+++ BlogProjectWeb/Repositories/BlogPostRepository.cs
@@ -25,12 +25,32 @@ namespace BlogProjectWeb.Repositories {
             return await blogDbContext.BlogPosts.Include(x => x.Tags).ToListAsync();
          }
 
-        public Task<BlogPost?> GetAsync(Guid id) {
-            throw new NotImplementedException();
+        public async Task<BlogPost?> GetAsync(Guid id) {
+            return await blogDbContext.BlogPosts.Include(x => x.Tags).FirstOrDefaultAsync(x => x.Id == id);
         }
 
-        public Task<BlogPost?> UpdateAsync(BlogPost blogPost) {
-            throw new NotImplementedException();
+        public async Task<BlogPost?> UpdateAsync(BlogPost blogPost) {
+
+            var existingBlog = await blogDbContext.BlogPosts.Include(x => x.Tags)
+                .FirstOrDefaultAsync(x => x.Id == blogPost.Id);
+
+            if (existingBlog != null) { 
+                existingBlog.Id = blogPost.Id;
+                existingBlog.Heading = blogPost.Heading;
+                existingBlog.PageTitle = blogPost.PageTitle;
+                existingBlog.Content = blogPost.Content;
+                existingBlog.ShortDescription = blogPost.ShortDescription;
+                existingBlog.Author = blogPost.Author;
+                existingBlog.FeaturedImageUrl = blogPost.FeaturedImageUrl;
+                existingBlog.UrlHandle = blogPost.UrlHandle;
+                existingBlog.Visible = blogPost.Visible;
+                existingBlog.PublishedDate = blogPost.PublishedDate;
+                existingBlog.Tags = blogPost.Tags;
+
+                await blogDbContext.SaveChangesAsync();
+                return existingBlog;
+            }
+            return null;
         }
     }
 }
